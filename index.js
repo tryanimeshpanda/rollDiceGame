ef9246d0a36d03843f63342dc5f06537a159e0cd
tryanimeshpanda <120265053+tryanimeshpanda@users.noreply.github.com>
2023-01-16 19:47:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var rand1=Math.random();
 var rand2=Math.random();
 var randomNumber1=Math.floor(rand1*6)+1;
 var randomNumber2=Math.floor(rand2*6)+1;
-var randomImage1="images/dice"+randomNumber1+".png";
-var randomImage2="images/dice"+randomNumber2+".png";
+var randomImage1="dice"+randomNumber1+".png";
+var randomImage2="dice"+randomNumber2+".png";
 document.querySelector(".img1").setAttribute("src",randomImage1);
 document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
 if(randomNumber1>randomNumber2)
